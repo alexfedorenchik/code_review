236833abc99e062d5147101cb70b64b1700b7525
Alexander Fedorenchik <alexander.fedorenchik@gmail.com>
2020-09-09 19:29:15 +0300

    Update IteratorTask.java
    Example comment added

--- src/com/company/IteratorTask.java
+++ src/com/company/IteratorTask.java
@@ -3,7 +3,10 @@ package com.company;
 import java.util.Iterator;
 
 public class IteratorTask {
-
+    
+    //left - 0,1,2,3
+    //right - 1,1,2,2
+    //output - 0,1,1,1,2,2,2,3
     public static Iterator<Integer> mergeSortedIterators(Iterator<Integer> left, Iterator<Integer> right) {
         return
     }
